cce203b36766603f1ffeffbf1d4951a572351d4a
Flavyoo <flavioandrade1918@gmail.com>
2022-01-02 15:04:00 -0500

    Styling for Legend UL and LI

--- src/components/LegendControls/LegendControls.tsx
+++ src/components/LegendControls/LegendControls.tsx
@@ -1,15 +1,35 @@
 import * as React from 'react';
 
 import { LegendControlProps } from './LegendControls.props';
+import { styled } from '@mui/material';
+
+const ControlUL = styled('ul')`
+  position: absolute;
+  text-align: center;
+  width: 100%;
+  z-index: 1;
+`;
+
+const ControlLI = styled('li')`
+  display: inline-bloack;
+  cursor: pointer;
+  margin: 0 0.5rem;
+  border-radius: 50%;
+  transition: opacity 0.25s ease-in;
+  opacity: 0.3;
+  box-shadow: 1px 1px 2px rgb(0 0 0/90%);
+  width: 10px;
+  height: 10px;
+`;
 
 export const LegendControls: React.FC<LegendControlProps> = ({
   numberOfControls,
 }) => {
   return (
-    <ul>
+    <ControlUL>
       {[...Array(numberOfControls)].map((_, i) => (
-        <li key={i}>{`Control ${i}`}</li>
+        <ControlLI key={i} />
       ))}
-    </ul>
+    </ControlUL>
   );
 };
