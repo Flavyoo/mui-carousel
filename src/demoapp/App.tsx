0fa75b12d874214a44eb67a91583fa2520804509
Flavyoo <flavioandrade1918@gmail.com>
2022-01-02 16:13:19 -0500

    Continue work on test app

--- src/demoapp/App.tsx
+++ src/demoapp/App.tsx
@@ -1,15 +1,25 @@
 import * as React from 'react';
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import { Carousel } from '../components';
+import { styled } from '@mui/material/styles';
+
+const StyledContainer = styled(Container)({
+  position: 'relative',
+  backgroundColor: '#ddd',
+  height: '100%',
+});;
 
 export default class App extends React.Component {
   render() {
     return (
-      <Container maxWidth="lg">
-        <Carousel>
-          <h1>Hello World</h1>
-        </Carousel>
-      </Container>
+        <StyledContainer maxWidth="lg">
+          <Carousel>
+            <Typography variant="h1">What</Typography>
+            <Typography variant="h1">Why</Typography>
+            <Typography variant="h1">When</Typography>
+            <Typography variant="h1">Who</Typography>
+          </Carousel>
+        </StyledContainer>
     );
   }
 }
