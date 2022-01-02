64a8d8aa9544ce10b5a38c4d9be1ae998d77a487
Flavyoo <flavioandrade1918@gmail.com>
2022-01-02 14:25:28 -0500

    class setup

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,7 +1,20 @@
 import * as React from 'react';
 
 import { CarouselProps } from './Carousel.props';
+import { styled } from '@mui/material/styles';
+
+const Root = styled('div')`
+  width: '100%';
+`;
+
+const CarouselItem = styled('li')``;
 
 export const Carousel: React.FC<CarouselProps> = ({ id, children }) => {
-  return <div id={id}>{children}</div>;
+  return (
+    <Root id={id}>
+      {React.Children.map(children, item => (
+        <CarouselItem>{item}</CarouselItem>
+      ))}
+    </Root>
+  );
 };
